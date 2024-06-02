4a0e9d4a8c559d33d0cc336e9342e21d9e25f2f9
Onur Anıl <onuranil749@gmail.com>
2024-06-03 01:20:26 +0300

    Initial Commit

---
+++ AlphaToDoApp/AlphaToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaToDoAppApp.swift
+//  AlphaToDoApp
+//
+//  Created by Onur Anıl on 3.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlphaToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlphaToDoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlphaToDoApp
+//
+//  Created by Onur Anıl on 3.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
